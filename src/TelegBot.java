aa0662defd2b402c993e45fdbcc5d6dfa1ed4af1
Arseniy <arszorin@gmail.com>
2016-12-09 02:18:08 +0300

    Initial commit

---
+++ src/TelegBot.java
@@ -0,0 +1,59 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * Created by arseniy on 09.12.16.
+ */
+class TelegBot extends TelegramLongPollingBot{
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new TelegBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()){
+            if (message.getText().equals("/help"))
+                sendMsg(message, "ТТ");
+            else
+                sendMsg(message, "ТТ");
+
+
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "arszorin_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "327648179:AAEC8acuq81q7sKwodfkYrdpLLqEhilRwMU";
+    }
+
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
